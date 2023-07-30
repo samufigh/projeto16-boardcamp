d50dc6930341224ff2bf608c4cc1488372817d6a
samufigh <samufigh21@gmail.com>
2023-07-30 09:25:29 -0300

    feat: schema post/games

--- controllers/games.controller.js
+++ controllers/games.controller.js
@@ -1,10 +1,9 @@
-
 import { db } from "../src/database/database.connection.js"
 
 export async function getGames(req, res){
     try {
         const games = await db.query(`SELECT * FROM games;`)
-        res.send(games.rows);
+        res.send(games.rows)
     } catch (err) {
         res.status(500).send(err.message)
     }
@@ -12,11 +11,22 @@ export async function getGames(req, res){
 
 export async function postGames(req, res){
     try {
-        const {name, image, stockTotal, pricePerDay} = req.body;
+        const {name, image, stockTotal, pricePerDay} = req.body
+        const games = await db.query(`SELECT name FROM games;`)
+        let exist = false
+        games.rows.forEach(game => {
+            if (game.name===name){
+                exist = true
+            }
+        })  
+        if(exist) return res.status(409).send("nome já existe") 
+
         await db.query(`
-        INSERT INTO games (name, image, "stockTotal", "pricePerDay")
-        VALUES ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])
-        res.sendStatus(201);
+        INSERT INTO games 
+        (name, image, "stockTotal", "pricePerDay")
+        VALUES
+        ($1, $2, $3, $4);`, [name, image, stockTotal, pricePerDay])           
+        res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import { getGames, postGames } from "../../controllers/games.controller.js";
+import { schemaGames } from "../../schemas/games.schemas.js";
+import { validateSchema } from "../middlewares/validateSchema.js";
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames);
-gamesRouter.post("/games", postGames);
+gamesRouter.post("/games", validateSchema(schemaGames), postGames);
+
 
 export default gamesRouter
