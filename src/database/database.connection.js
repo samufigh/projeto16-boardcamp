7a13b8a0717754aeac17a82bd5198e036fb87dc9
samufigh <samufigh21@gmail.com>
2023-07-30 22:07:43 -0300

    feat: configurações de deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -8,6 +8,6 @@ const configDatabase = {
   connectionString: process.env.DATABASE_URL,
 };
 
-if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+if (process.env.MODE === "prod") configDatabase.ssl = true;
 
 export const db = new Pool(configDatabase);
